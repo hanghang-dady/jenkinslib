f1f551fc23f054a752cb0fa730ed4473217d09cc
hanghang-dady <43260062+hanghang-dady@users.noreply.github.com>
2020-06-20 15:52:28 +0800

    增加打印日志格式信息

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,7 +1,18 @@
 package org.devops
 
-//打印内容
-def PrintMes(content){
-    println(content)
+//打印日志内容
+//def  PrintMes(content){
+//     println(content)
+//}
 
+//格式化日志输出
+def PrintMes(value,color){
+    color = ['red'   : "\033[40;31m >>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<<< \033[0m",
+             'blue'  : "\033[47;34m ${value}  \033[0m",
+             'green' : ".[1;32m>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<<<.[m",
+             'green1' : "\033[40;32m >>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<<< \033[0m"  ]
+            
+    ansiColor('xterm'){
+        println(colors[color])
+    }
 }
