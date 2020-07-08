faccc75d116492c4cb2536007c98f0e7a0ddff81
hanghang-dady <43260062+hanghang-dady@users.noreply.github.com>
2020-07-08 11:30:50 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -5,7 +5,14 @@ def Build(buildType,buildShell){
     def buildTools = ["mvn":"M3","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
     
     println("当前选择的构建类型为 ${buildType}")
-    buildHome= tool budileTools[buildType]
+    buildHome= tool buildTools[buildType]
+    
+    if ("${buildType}" == "npm"){
+        sh """
+            export NODE_HOME=${buildHome}
+            export PATH=\$NODE_HOME/bin:\$PATH
+               ${buildHome}/bin/${buildType}  ${buildShell} """
+    }
     
     sh "${buildHome}/bin/${buildType}  ${buildShell}"
 }
